88dae0a0bb642486634ca4e7ae13bd3661c13eec
Zaeem Khan <zaeembaloch1198@gmail.com>
2023-04-20 06:18:17 +0500

    added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Electronics from "./electronics";
+import Menware from "./menware";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="bg-gray-200 py-4">
+           <ul className="flex space-x-10 items-center justify-center">
+            <li><h1 className="font-bold text-4xl md:mr-[5rem] lg:mr-[30rem]">SHOPLY</h1></li>
+            <li className="bg-gray-300 hover:bg-gray-400 p-2 rounded hover:text-white">All Products</li>
+            <li className="bg-gray-300 hover:bg-gray-400 p-2 rounded hover:text-white">About Us</li>
+            <li className="bg-gray-300 hover:bg-gray-400 p-2 rounded hover:text-white">Contact</li>
+            <li className="bg-gray-300 hover:bg-gray-400 p-2 rounded hover:text-white">Cart</li>
+           </ul>
       </header>
+      <body className="text-center w-full h-96 bg-gradient-to-r from-blue-200 to-green-200 justify-center content-center grid">
+                <div>
+                  <p className="inline text-3xl font-bold">Shopping made easy by Shoply</p>
+                  <p>We provide best quality products to our customers at best prices</p>
+                  <p>Shop with us and get excitig discounts!</p>
+                  <button className="bg-gradient-to-r text-2xl from-blue-400 to-green-400 p-5 m-7 rounded-2xl text-white">Shop Now</button>
+                </div>  
+      </body>
+      <h1 className="text-center p-10 text-2xl font-bold">Best Selling Products</h1>
+       <Electronics/>
+       <Menware/>
     </div>
   );
 }
